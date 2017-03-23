e351ec979c8432abf8e85b5f8121e8bbe1be753b
Istvan Fazakas <ifazakas@gmail.com>
2017-03-23 15:46:41 +0100

    Simplify controller test setup

--- src/test/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/common/ControllerTestBase.java
+++ src/test/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/common/ControllerTestBase.java
@@ -1,29 +1,18 @@
 package com.ifazakas.mybookshelfapi.infrastructure.springboot.controller.common;
 
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest
 public abstract class ControllerTestBase {
   protected static final MediaType MEDIA_TYPE = MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE);
   @Autowired
-  private WebApplicationContext context;
   private MockMvc server;
-
-  @Before
-  public void setup() {
-    server = MockMvcBuilders
-        .webAppContextSetup(context)
-        .build();
-  }
 
   protected MockMvc server() {
     return server;
