9f562f69cfb83c96f33b88b2dd0ac2ebca744a82
Istvan Fazakas <ifazakas@gmail.com>
2017-03-23 13:10:19 +0100

    Fix Gradle bootRun

--- src/main/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/Application.java
+++ src/main/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.ifazakas.mybookshelfapi;
+package com.ifazakas.mybookshelfapi.infrastructure.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
