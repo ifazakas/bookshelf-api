8018a8aaefff67986eccecdc6d717a6b56dcc18d
Istvan Fazakas <ifazakas@gmail.com>
2017-03-24 18:37:16 +0100

    Test controllers loaded in Spring context

--- src/main/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/AddBookController.java
+++ src/main/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/AddBookController.java
@@ -1,5 +1,10 @@
 package com.ifazakas.mybookshelfapi.infrastructure.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
 public class AddBookController {
 
 
--- src/test/java/com/ifazakas/mybookshelfapi/ApplicationTests.java
+++ src/test/java/com/ifazakas/mybookshelfapi/ApplicationTests.java
@@ -1,17 +1,29 @@
 package com.ifazakas.mybookshelfapi;
 
 import com.ifazakas.mybookshelfapi.infrastructure.springboot.Application;
+import com.ifazakas.mybookshelfapi.infrastructure.springboot.controller.AddBookController;
+import com.ifazakas.mybookshelfapi.infrastructure.springboot.controller.SayHelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
+  @Autowired
+  private SayHelloController sayHelloController;
+  @Autowired
+  private AddBookController addBookController;
+
   @Test
   public void contextLoads() {
+    assertThat(sayHelloController).isNotNull();
+    assertThat(addBookController).isNotNull();
   }
 
 }
