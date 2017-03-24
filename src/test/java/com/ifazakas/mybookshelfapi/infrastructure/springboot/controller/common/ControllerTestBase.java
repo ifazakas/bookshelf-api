23c1571e0fb1af5dac6d8154efa86641d1c7d21a
Istvan Fazakas <ifazakas@gmail.com>
2017-03-24 18:41:37 +0100

    Add AddBook controller acceptance failing test

--- src/test/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/common/ControllerTestBase.java
+++ src/test/java/com/ifazakas/mybookshelfapi/infrastructure/springboot/controller/common/ControllerTestBase.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -18,4 +24,18 @@ public abstract class ControllerTestBase {
     return server;
   }
 
+  protected String getResourceAsString(String resource) {
+    Scanner scanner = new Scanner(
+        getClass().getClassLoader().getResourceAsStream(resource),
+        Charset.forName("UTF-8").toString()
+    ).useDelimiter("\\A");
+    return scanner.hasNext() ? scanner.next() : "";
+  }
+
+  protected MockHttpServletRequestBuilder createPostRequest(String endpoint, String body) {
+    return post(endpoint)
+        .accept(MEDIA_TYPE)
+        .contentType(MEDIA_TYPE)
+        .content(body);
+  }
 }
